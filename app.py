84840f97b11334a33e7df2ce504d5633dc0b793a
Rein <reinbugnot@gmail.com>
2023-08-15 23:09:10 +0800

    working front-end code

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template("index.html")
+    if request.method == 'POST':
+        cvtext = request.form['cvtext']
+        jdtext = request.form['jdtext']
+        charcount = request.form['charcount']
+        
+        # Some function
+        output = cvtext + jdtext + charcount
+        
+        return render_template('index.html', output=output)
+    else:
+        return render_template('index.html', output='')
 
-@app.route('/output', methods=['POST'])
-def output():
-    cvtext = request.form.get('cvtext')
-    jdtext = request.form.get('jdtext')
-    targetcharcount = request.form.get('targetcharcount')
-    return render_template("output.html")
 
 if __name__ == '__main__':
 	app.run()
