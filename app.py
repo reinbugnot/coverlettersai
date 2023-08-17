fdc16d47d783b9b4b6177a2d47c418cea84ffde9
Rein <reinbugnot@gmail.com>
2023-08-17 09:07:38 +0800

    feat: added backend code

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, flash
+from model.model_functions import get_completion, get_coverletter
+from langchain.llms import OpenAI
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+llm = OpenAI(openai_api_key=openai.api_key)
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -6,17 +13,18 @@ app.secret_key = "manbearpig_MUDMAN888"
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        cvtext = request.form['cvtext']
-        jdtext = request.form['jdtext']
-        charcount = request.form['charcount']
+        cv = request.form['cvtext']
+        jd = request.form['jdtext']
+        word_count = int(request.form['charcount'])
         
         # Some function
-        output = cvtext + jdtext + charcount
+        app.logger.info('Generating Cover Letter...')
+        output = get_coverletter(cv, jd, word_count)
         
         return render_template('index.html', output=output)
     else:
-        return render_template('index.html', output='')
+        return render_template('index.html', output='Input Error, please try again')
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
---
+++ model/model_functions.py
@@ -0,0 +1,39 @@
+import openai
+import os
+import pandas as pd
+import numpy as np
+
+def get_completion(input_prompt, model="gpt-3.5-turbo", temperature = 0.4, system_prompt_fpath = './model/prompt-inventory/system-prompt.txt',):
+    
+    with open(system_prompt_fpath, 'r') as file:
+        system_prompt = file.read()
+    
+    messages = [{"role": "system", "content": system_prompt},
+                {"role": "user", "content": input_prompt},
+               ]
+    
+    response = openai.ChatCompletion.create(model=model,
+                                            messages=messages,
+                                            temperature=temperature
+                                           )
+    return response.choices[0].message["content"]
+
+def get_coverletter(cv, jd, word_count, prompt_head_fpath = './model/prompt-inventory/prompt-head.txt'):
+    
+    # Load prompt head
+    with open(prompt_head_fpath, 'r') as file:
+        input_prompt_head = file.read()
+
+    # Construct full input prompt
+    input_prompt = f"""{input_prompt_head} 
+
+    You are REQUIRED to limit your output to only within {int(word_count * 0.80)} words.
+
+    My first input is:
+
+    >>>{cv}<<< 
+
+    ###{jd}###"""
+    
+    return get_completion(input_prompt)
+    
