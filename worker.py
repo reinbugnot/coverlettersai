83b0a39b0314124a878e4a7706e88c299665e1a7
Rein <reinbugnot@gmail.com>
2023-08-18 16:13:52 +0800

    'fix: connection to redis'

--- worker.py
+++ worker.py
@@ -3,7 +3,7 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL') #, 'redis://localhost:6379') #---> for local testing
 
 conn = redis.from_url(redis_url)
 
